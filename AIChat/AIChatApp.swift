a8fad339c35a657a6e09c3fe48894c20e5bbd25a
Youssef Mohamed <youssef.mohamed.ib@gmail.com>
2026-03-02 00:13:46 +0200

    Initial Commit

---
+++ AIChat/AIChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIChatApp.swift
+//  AIChat
+//
+//  Created by Youssef Mohamed on 02/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct AIChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
