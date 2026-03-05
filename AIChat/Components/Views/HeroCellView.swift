191f4bc38524d6fe7d50e4f11909b8118c2198d0
Youssef Mohamed <youssef.mohamed.ib@gmail.com>
2026-03-05 17:35:51 +0200

    [Feature] Add HeroCellView

---
+++ AIChat/Components/Views/HeroCellView.swift
@@ -0,0 +1,18 @@
+//
+//  HeroCellView.swift
+//  AIChat
+//
+//  Created by Youssef Mohamed on 05/03/2026.
+//
+
+import SwiftUI
+
+struct HeroCellView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HeroCellView()
+}
