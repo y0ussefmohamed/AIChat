42600f9b11923c938af67dc97e330782099fc783
Youssef Mohamed <youssef.mohamed.ib@gmail.com>
2026-03-06 06:38:47 +0200

    [Feature] Adding CarouselView

---
+++ AIChat/Components/Views/CarouselView.swift
@@ -0,0 +1,18 @@
+//
+//  CarouselView.swift
+//  AIChat
+//
+//  Created by Youssef Mohamed on 06/03/2026.
+//
+
+import SwiftUI
+
+struct CarouselView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CarouselView()
+}
